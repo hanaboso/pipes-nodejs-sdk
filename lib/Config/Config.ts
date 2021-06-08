a51161e5fafa17c409b1fd1cdf53a8bdb1f81452
Radek Jirsa <jirsa.r@ghanaboso.com>
2021-06-08 17:32:57 +0200

    Ci

--- lib/Config/Config.ts
+++ lib/Config/Config.ts
@@ -1,17 +1,17 @@
 export const loggerOptions = {
-  server: process.env.UDP_LOGGER_HOST || 'localhost',
+  server: process.env.UDP_LOGGER_HOST || '127.0.0.40',
   port: parseInt(process.env.UDP_LOGGER_PORT || '5120', 10),
 };
 
 export const metricsOptions = {
-  server: process.env.METRICS_HOST || 'localhost',
+  server: process.env.METRICS_HOST || '127.0.0.40',
   port: parseInt(process.env.METRICS_PORT || '4444', 10),
   curlMeasurement: process.env.CURL_METRICS_MEASUREMENT || 'monolith',
   processMeasurement: process.env.PROCESS_METRICS_MEASUREMENT || 'connectors',
 };
 
 export const storageOptions = {
-  dsn: process.env.MONGO_DSN || 'mongodb://localhost:27017',
+  dsn: process.env.MONGO_DSN || 'mongodb://127.0.0.40:27017',
 };
 
 export const appOptions = {
